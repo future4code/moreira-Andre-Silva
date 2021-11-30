cb5e0f3a1f97f951e5271b823c25fb801bdec0e2
André Guimarães Gonçalves da Silva <94801775+andre-guimaraes11@users.noreply.github.com>
2021-11-30 11:54:14 -0300

    exercicio-variaveis

--- modulo1/variaveis/template_javascript/index.js
+++ modulo1/variaveis/template_javascript/index.js
@@ -1,6 +1,6 @@
 
-/*"  QUESTÃO 1"
-let a = 10
+"  QUESTÃO 1"
+let a = 15
 let b = 10
 
 console.log(b)
@@ -10,12 +10,14 @@ console.log(a, b)
 /*Executa as váriaveis na primeira linha com o valor 10 
 na segunda linha como 10, 5*/
 
-/*"Questão 2"
+"Questão 2"
 let a = 10
 let b = 20
 c = a
 b = c
 a = b
 
-/*Executa as váriaveis com o mesmo valor, sendo calculado o valor médio  
-console.log(a, b, c)*/
+ 
+console.log(a, b, c)
+
+/*Executa as váriaveis com o mesmo valor, sendo calculado o valor médio*/
