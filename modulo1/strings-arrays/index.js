c74df40c28069bf6df7d9de67fa1c4c9d6d3205d
André Guimarães Gonçalves da Silva <94801775+andre-guimaraes11@users.noreply.github.com>
2021-12-10 13:20:56 -0300

    strings-arrays-concluido

--- modulo1/strings-arrays/index.js
+++ modulo1/strings-arrays/index.js
@@ -1,7 +1,7 @@
 // Exercícios interpretação de Códigos
 
-//QUESTÃO 1
-//a. Criado uma variávvel com nome array, executado no console com o texto a. com valor undefined, no caso 
+//1
+//a. Criado uma variável com nome array, executado no console com o texto a. com valor undefined, no caso 
 // é indefinido
 
 //b. É atribuído o valor null para a váriavel e exibido como b. null
@@ -19,19 +19,59 @@
 //f. É criado uma nova variável como valor e somado a variável i com o index 6
 
 
-//QUESTÃO 2
+// 2
 
 // O valor impresso no console com os comandos do exercício seriam deixar as letras maíusculas, trocar as letras A
 //por I, mostrar o tamanho da frase
 
 //Subi num ônibus em Marrocos// 
 //SUBI NUM ÔNIBUS EM MIRROCOS com o tamanho da frase// esse seria o resultado da frase 
+// 
+//Exercícios de escrita de código
+// 
+// 1)
+// const nomeDoUsuario = prompt("Qual seu nome?")
+// const emailDolUsuario = prompt("Qual seu e-mail")
 
-function minhaFunção(variavel) {
-return variavel *5
-}
+// console.log(`O e-mail ${emailDolUsuario} foi cadastrado com sucesso. Seja bem-vinda(o), ${nomeDoUsuario}!`)
+// // 2) 
 
-console.log(minhaFunção(2))
-console.log(minhaFunção(10))
+// let comidaFavorita = ["Pizza", "Lasanha", "X-burguer", "Coxinha ", "Laranja"]
+// console.log(comidaFavorita)
+// console.log("Essas são as minhas comidas favoritas:",comidaFavorita,)
 
+// let comidaUsuario = prompt(`Qual sua comida favorita?`)
+// const trocarComida = [comidaUsuario]
+// const comidaTrocada = trocarComida.toString()
+// comidaFavorita[1] = comidaTrocada
+// console.log(comidaFavorita)
 
+// 3)
+
+const listaDeTarefas = []
+
+const perguntaUm =   prompt("Quais sua primeira tarefa do dia")
+const respostaUm = [perguntaUm]
+const trocaUm = respostaUm.toString()
+
+listaDeTarefas[0] = trocaUm
+
+const perguntaDois = prompt("Quais sua segunda tarefa do dia")
+const respostaDois = [perguntaDois]
+const trocaDois = respostaDois.toString()
+
+listaDeTarefas[1] = trocaDois
+
+const perguntaTres = prompt("Quais sua terceira tarefa do dia")
+const respostaTres = [perguntaTres]
+const trocaTres = respostaTres.toString()
+
+listaDeTarefas[2] = trocaTres
+
+console.log(listaDeTarefas)
+
+const tirarTarefa = Number(prompt("Escolha um número de 0 até 2"))
+
+listaDeTarefas.splice(tirarTarefa, 1)
+
+console.log(listaDeTarefas)
